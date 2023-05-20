cadbc183c3c6d7db5dd4d88fac3c3fbd482600eb
“Aleksandr <ross1990@mail.ru>
2023-05-21 00:17:28 +0300

    before review2

--- measurement/models.py
+++ measurement/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# TODO: опишите модели датчика (Sensor) и измерения (Measurement)
+
+class Sensor(models.Model):
+    name = models.CharField(max_length=50, verbose_name='Название', null=False)
+    description = models.CharField(max_length=300, verbose_name='Описание')
+
+    def __str__(self):
+        return self.name
+
+
+class Measurement(models.Model):
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE, related_name='measurements',
+                               verbose_name='Модель датчика')
+    temperature = models.FloatField(verbose_name='Температура')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='Дата измерения')
+    image_model = models.ImageField(upload_to='images/', null=True, blank=True, max_length=255)
--- measurement/views.py
+++ measurement/views.py
@@ -1,2 +1,40 @@
-# TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
-# TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from rest_framework.response import Response
+from .models import Sensor, Measurement
+from .serializers import SensorDetailSerializer, SensorSerializer, MeasurementSerializer
+
+
+class SensorView(ListAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+    def post(self, request):
+        name = request.data.get('name')
+        description = request.data.get('description')
+        Sensor(name=name, description=description).save()
+        return Response({'status': 'Готово'})
+
+
+class ChangeSensorView(RetrieveAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
+
+    def patch(self, request, pk):
+        object = Sensor.objects.get(id=pk)
+        if request.data.get('name'):
+            object.name = request.data.get('name')
+        if request.data.get('description'):
+            object.description = request.data.get('description')
+        object.save()
+        return Response({'status': 'Готово'})
+
+
+class MeasurementView(ListAPIView):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
+
+    def post(self, request):
+        sensor = Sensor.objects.get(id=request.data.get('sensor'))
+        temperature = request.data.get('temperature')
+        Measurement(sensor=sensor, temperature=temperature).save()
+        return Response({'status': 'готово'})
